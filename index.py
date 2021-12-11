023ef1924f30d4a9965041e72b81e5aa0875fc6f
shahnawazahmed303 <shahnawazahmed303@gmail.com>
2021-12-11 15:41:00 +0530

    img

--- index.py
+++ index.py
@@ -1,21 +1,23 @@
 from tkinter import *
-
+from PIL import Image , ImageTk
 root = Tk() #Referance to TK()
 
 #Width x Height
 root.geometry("644x434")
 
 #width, height
-root.minsize(300,100) # min size of window
-root.maxsize(1200,988) # max size of window
+root.minsize(400,400) # min size of window
+root.maxsize(800,800) # max size of window
 
 #label 
-# label = Label(text="Hello ji")
-# label.pack() #must do else not work
+label = Label(text="Hello ji")
+label.pack() #must do else not work
 
 #img
 
-img_label = Label(image = PhotoImage(file="1.svg"))
+photo = Image.open("img/1.png")
+img = ImageTk.PhotoImage(photo)
+img_label = Label(image=img)
 img_label.pack()
 
 # main window tk
