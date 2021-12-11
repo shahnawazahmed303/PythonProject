3b6d611b9c470e4ccedd065a13805019ad400547
shahnawazahmed303 <shahnawazahmed303@gmail.com>
2021-12-11 15:27:27 +0530

    img

--- index.py
+++ index.py
@@ -10,14 +10,13 @@ root.minsize(300,100) # min size of window
 root.maxsize(1200,988) # max size of window
 
 #label 
-label = Label(text="Hello ji")
-label.pack() #must do else not work
+# label = Label(text="Hello ji")
+# label.pack() #must do else not work
 
 #img
 
-photo = PhotoImage(file="C:\Users\ShahNawazAhmed\Documents\PythonProject\img\1.png")
-
-img_label = Label(image=photo)
+img_label = Label(image = PhotoImage(file="1.svg"))
 img_label.pack()
 
-root.mainloop() # main window tk
+# main window tk
+root.mainloop() 
